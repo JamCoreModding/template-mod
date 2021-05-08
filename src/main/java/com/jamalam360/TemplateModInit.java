f77902a2c23f7e97193116856966592418fadab9
James Ellison <jamesellison2006@gmail.com>
2021-05-08 12:19:14 +0100

    code

---
+++ src/main/java/com/jamalam360/TemplateModInit.java
@@ -0,0 +1,26 @@
+package com.jamalam360;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TemplateModInit implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "templatemod";
+    public static final String MOD_NAME = "TemplateMod";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
