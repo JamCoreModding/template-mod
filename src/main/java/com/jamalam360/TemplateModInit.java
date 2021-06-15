ba2de21d01978f09cc58aa4528903742901329aa
James Ellison <jamesellison2006@gmail.com>
2021-06-15 19:18:37 +0100

    -Updates to 1.17 -Adds auto-license plugin -Adds a few more sections in fabric.mod.json -Updates compat levels in build.gradle and templatemod.mixins.json

--- src/main/java/com/jamalam360/TemplateModInit.java
+++ src/main/java/com/jamalam360/TemplateModInit.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Jamalam360
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.jamalam360;
 
 import net.fabricmc.api.ModInitializer;
@@ -10,11 +34,11 @@ public class TemplateModInit implements ModInitializer {
     public static Logger LOGGER = LogManager.getLogger();
 
     public static final String MOD_ID = "templatemod";
-    public static final String MOD_NAME = "TemplateMod";
+    public static final String MOD_NAME = "Template Mod";
 
     @Override
     public void onInitialize() {
-        log(Level.INFO, "Initializing " + MOD_NAME + " under the ID '" + MOD_ID + "'");
+        log(Level.INFO, "Initializing '" + MOD_NAME + "' under the ID '" + MOD_ID + "'");
     }
 
     public static void log(Level level, String message){
